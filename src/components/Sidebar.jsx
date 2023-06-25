e4dfd29044c89c395b5ef02c16d8ddb866e09553
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-06-24 22:46:01 -0300

    Add button signout in the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,12 +5,14 @@ import {
   Calendar,
   ChartBar,
   House,
+  SignOut,
   Student,
   Users,
   UsersFour,
   Wallet,
   X,
 } from '@phosphor-icons/react'
+import { signOut } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
@@ -92,6 +94,12 @@ export default function Sidebar() {
             </li>
           ))}
         </ul>
+        <button
+          className="group flex transform cursor-pointer items-center gap-x-2 rounded-md py-2 pl-2 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out hover:scale-105 hover:bg-[#1D70B6] hover:fill-current hover:text-gray-200 focus:outline-none"
+          onClick={() => signOut()}
+        >
+          <SignOut size={24} />
+        </button>
       </aside>
     </div>
   )
