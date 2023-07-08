fa0d5cffd8bad4c9723c2192cf86c3cc4310450b
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-07-08 10:22:46 -0300

    Add provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,29 +1,39 @@
 import type { NextAuthOptions } from 'next-auth'
-// import CredentialsProvider from 'next-auth/providers/credentials'
+import CredentialProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    // CredentialsProvider({
-    //   name: 'Sign in',
-    //   credentials: {
-    //     email: {
-    //       label: 'Email',
-    //       type: 'email',
-    //       placeholder: 'example@example.com',
-    //     },
-    //     password: { label: 'Password', type: 'password' },
-    //   },
-    //   async authorize(credentials) {
-    //     const user = { id: '1', name: 'Admin', email: 'admin@admin.com' }
-    //     return user
-    //   },
-    // }),
+    CredentialProvider({
+      name: 'credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'example@example.com',
+        },
+        password: { label: 'Senha', type: 'password' },
+        name: {
+          label: 'Nome',
+          type: 'text',
+          placeholder: 'Seu Nome de Usuário',
+        },
+      },
+      async authorize(credentials, req): Promise<any> {
+        const user = {
+          email: 'admin@admin.com',
+          password: '123456',
+          name: 'Wellington Nadson',
+        }
+        return user
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  debug: process.env.NODE_ENV === 'development',
   // secret: process.env.NEXTAUTH_SCRET,
   pages: {
     signIn: '/login',
