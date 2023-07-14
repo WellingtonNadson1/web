538f1043d586a4d89e5a8c2ba19be8e1354486e2
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-07-14 19:07:36 -0300

    Type the session

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,7 +7,33 @@ declare module 'next-auth' {
     user: {
       id: string
       email: string
-      name: string
+      password: string
+      image: string
+      firstName: string
+      lastName: string
+      cpf: string
+      dateNasc: string | Date
+      sexo: string
+      telefone: string
+      escolaridade: string
+      profissao: string
+      batizado: string
+      dateBatizado: string | Date
+      isDiscipulado: string
+      discipulador: string
+      supervisao: string
+      celula: string | undefined
+      escolas: [id: string, nome: string]
+      encontros: [id: string, nome: string]
+      estadoCivil: string
+      nomeConjuge: string
+      dateCasamento: string | Date
+      hasFilho: string
+      quantidadeFilho: number
+      endereco_id: string | undefined
+      dateDecisao: string | Date
+      situacao_no_reino: string
+      cargo_de_lideranca: string
     }
   }
 }
